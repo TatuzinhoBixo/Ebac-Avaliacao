9ea30b92d657c32fa5e4ad96faf802df42bd4129
Marcelo Lima <celuw_lima@hotmail.com>
2023-11-18 21:21:29 -0400

    dsv-bootstrap falta:formulario

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    /*$('#nomeContato')
+    $('#email')*/
+    $('#telefone').mask('(00) 0000-0000')
+
+    $('form').validate ({
+        rules: {
+            nomeContato: {
+                required: true
+            },
+            email: {
+                required: true
+            },
+            telefone: {
+                required: true
+            }
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIcorretos} campos incorretos`)
+            }
+        }
+    })
+})
