cb13790304f28b83158a2b7fb96171cd44c4100f
Marcelo Lima <celuw_lima@hotmail.com>
2023-11-19 18:21:02 -0400

    bootstrap-1 FIM

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     /*$('#nomeContato')
     $('#email')*/
-    $('#telefone').mask('(00) 0000-0000')
+    $('#telefone').mask('(00) 00000-0000')
 
     $('form').validate ({
         rules: {
